ad95eb034334b77b225f25e5985e3c98a77b0a67
Ben Lovell <ben.j.lovell@gmail.com>
2013-11-14 00:47:38 +0000

    Initial twitter and naive bayes

---
+++ twitter.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+from secrets import *
+from twython import TwythonStreamer
+import train
+train.main()
+from colorama import init, Fore
+train.categories[0].colour = Fore.GREEN
+train.categories[1].colour = Fore.RED
+
+class SentimentStreamer(TwythonStreamer):
+    def on_success(self, data):
+        if 'text' in data:
+            mood = train.classify(data['text'])
+            if mood is None:
+                print data['text'].encode('utf-8')
+                print "neutral"
+            else: 
+                print mood.colour + data['text'].encode('utf-8') + Fore.RESET
+                print(mood)
+
+    def on_error(self, status_code, data):
+        print status_code
+
+stream = SentimentStreamer(APP_KEY, APP_SECRET, 
+           OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+stream.statuses.filter(locations=[-6.833496, 49.894634, 2.087402, 59.623325])
+
